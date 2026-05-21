3d42dbc54e303ee589e82243e41d06f191dbd655
Anne Phan <53896516+vtnphan@users.noreply.github.com>
2026-05-21 10:36:29 +1000

    fix: fix mako and update revision id

--- alembic/versions/20260521_103256_add_last_login_ip_to_app_users_91b54c7a79c5.py
+++ alembic/versions/20260521_103256_add_last_login_ip_to_app_users_91b54c7a79c5.py
@@ -1,19 +1,12 @@
-"""Add last_login_ip to app_users.
-
-Revision ID: e7f8a9b0c1d2
-Revises: c8d9e1f2a3b4
-Create Date: 2026-05-19 10:00:00.000000
-"""
-
-from __future__ import annotations
-
-import sqlalchemy as sa
+"""add_last_login_ip_to_app_users"""
 
 from alembic import op
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
-revision = "e7f8a9b0c1d2"
-down_revision = "c8d9e1f2a3b4"
+revision = '91b54c7a79c5'
+down_revision = 'c8d9e1f2a3b4'
 branch_labels = None
 depends_on = None
 
