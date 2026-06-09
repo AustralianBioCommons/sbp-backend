72cdbf6e953fc4ae950f343071cb5499e0c7a25b
Marius Mather <marius.mather@sydney.edu.au>
2026-06-10 09:34:27 +1000

    fix: merge the current alembic branches

---
+++ alembic/versions/20260610_092046__cb2033051aca.py
@@ -0,0 +1,19 @@
+"""empty message"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cb2033051aca'
+down_revision = ('3ef9d934712f', '60712a56f2c2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
