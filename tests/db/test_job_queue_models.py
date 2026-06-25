dcd0d92ec55af0078c2970baeca69419b205f428
Marius Mather <marius.mather@sydney.edu.au>
2026-06-25 11:04:39 +1000

    test: check preRunScript raise error in launch_payload

--- tests/db/test_job_queue_models.py
+++ tests/db/test_job_queue_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tests.datagen import QueuedJobFactory, WorkflowRunFactory
 
 
@@ -10,3 +12,15 @@ def test_job_queue_model(test_db, persistent_models):
     assert job.id is not None
     assert job.workflow_run_id is not None
     assert job.queued_at is not None
+
+
+def test_job_queue_no_prerun_script(test_db, persistent_models):
+    """
+    Test that the QueuedJob model does not allow launch_payload to include a preRunScript.
+    """
+    workflow_run = WorkflowRunFactory.create_sync()
+    with pytest.raises(ValueError):
+        QueuedJobFactory.create_sync(
+            workflow_run=workflow_run,
+            launch_payload={"preRunScript": "echo 'hello world'"}
+        )
