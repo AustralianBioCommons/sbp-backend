f554a639c3b4760f1ce5a08e44f217f4170313da
Marius Mather <marius.mather@sydney.edu.au>
2026-07-10 10:13:11 +1000

    fix: use replace_existing so we don't try to insert duplicates in the DB

--- app/run_scheduler.py
+++ app/run_scheduler.py
@@ -26,26 +26,23 @@ def main(dry_run: bool = False):
             max_instances=1,
             replace_existing=True,
         )
-        if not SCHEDULER.get_job("refresh_user_credits", jobstore="db"):
-            logger.info(
-                f"Adding monthly refresh_user_credits to scheduler: trigger = {MONTHLY_TRIGGER}"
-            )
-            logger.info(
-                f"Next refresh at: {MONTHLY_TRIGGER.get_next_fire_time(previous_fire_time=None, now=datetime.now(tz=UTC))}"
-            )
-            SCHEDULER.add_job(
-                refresh_user_credits,
-                kwargs={"dry_run": dry_run},
-                jobstore="db",
-                trigger=MONTHLY_TRIGGER,
-                id="refresh_user_credits",
-                misfire_grace_time=60 * 60 * 24,
-                max_instances=1,
-                replace_existing=False,
-                coalesce=True,
-            )
-        else:
-            logger.info("Monthly refresh_user_credits already exists in scheduler")
+        logger.info(
+            f"Adding monthly refresh_user_credits to scheduler: trigger = {MONTHLY_TRIGGER}"
+        )
+        logger.info(
+            f"Next refresh at: {MONTHLY_TRIGGER.get_next_fire_time(previous_fire_time=None, now=datetime.now(tz=UTC))}"
+        )
+        SCHEDULER.add_job(
+            refresh_user_credits,
+            kwargs={"dry_run": dry_run},
+            jobstore="db",
+            trigger=MONTHLY_TRIGGER,
+            id="refresh_user_credits",
+            misfire_grace_time=60 * 60 * 24,
+            max_instances=1,
+            replace_existing=True,
+            coalesce=True,
+        )
         logger.info("Starting scheduler")
         SCHEDULER.start()
     finally:
